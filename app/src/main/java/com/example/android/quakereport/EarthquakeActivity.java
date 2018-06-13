5e675e2c85572d289901e922eaf25beee7b88849
yaostyle <yaostyle@gmail.com>
2018-06-12 17:35:50 -0700

    Added ConnectivityManager to check network connectivity status.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,7 +16,11 @@
 package com.example.android.quakereport;
 
 import android.content.Intent;
+import android.net.Network;
 import android.net.Uri;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -100,10 +104,23 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         task.execute(USGS_REQUEST_URL);
         */
 
-        LoaderManager loaderManager = getLoaderManager();
+        //LoaderManager loaderManager = getLoaderManager();
 
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
-        Log.e(LOG_TAG, "Init the loader..");
+        //loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        //Log.e(LOG_TAG, "Init the loader..");
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        } else {
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
     }
 
     /* No longer needed
